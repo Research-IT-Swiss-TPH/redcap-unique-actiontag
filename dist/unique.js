c727f84fa628a9da29559212b352f6d387bc0a1a
tertek <ekin.tertemiz@swisstph.ch>
2024-03-25 11:47:45 +0100

    Add typed base object in window context.

--- dist/unique.js
+++ dist/unique.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello World 2");
+window.STPH_UAT.log = function () {
+    if (!this.params.show_debug)
+        return;
+    switch (arguments.length) {
+        case 1:
+            console.log(arguments[0]);
+            return;
+        case 2:
+            console.log(arguments[0], arguments[1]);
+            return;
+        case 3:
+            console.log(arguments[0], arguments[1], arguments[2]);
+            return;
+        case 4:
+            console.log(arguments[0], arguments[1], arguments[2], arguments[3]);
+            return;
+        default:
+            console.log(arguments);
+    }
+};
--- src/unique.ts
+++ src/unique.ts
@@ -1,2 +1,43 @@
+interface Window  {
+    STPH_UAT: UniqueActionTagData
+}
 
-console.log("Hello World 2");
+interface UniqueActionTagData {
+    data: {
+        fields: {}
+        errors: {
+            not_allowed_flat: string[]
+            not_allowed_multiple: string[]
+        }
+    },
+    params: {
+        show_debug: boolean,
+        show_erors: boolean,
+        show_labels: boolean,
+        enable_hard_check: boolean
+    },
+    log: Function
+}
+
+window.STPH_UAT.log = function() {
+
+    if(!this.params.show_debug) return;
+
+    switch(arguments.length) {
+        case 1: 
+            console.log(arguments[0]); 
+            return;
+        case 2: 
+            console.log(arguments[0], arguments[1]); 
+            return;
+        case 3: 
+            console.log(arguments[0], arguments[1], arguments[2]); 
+            return;
+        case 4:
+            console.log(arguments[0], arguments[1], arguments[2], arguments[3]); 
+            return;
+        default:
+            console.log(arguments);
+    }
+}
+
